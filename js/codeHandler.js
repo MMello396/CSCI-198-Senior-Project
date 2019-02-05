23a56569981792eca4a5d7a3c24c07005147351b
Matthew Mello <matthewmello396@gmail.com>
2019-02-04 19:12:00 -0800

    Im bad at source control...

--- js/codeHandler.js
+++ js/codeHandler.js
@@ -1,13 +1,4 @@
-var probTitle = document.getElementById("title").innerHTML
-var probObj = document.getElementById("objective").innerHTML
-var probTask = document.getElementById("task").innerHTML
-var probInputFormat = document.getElementById("inputFormat").innerHTML
-var probConstraints = document.getElementById("constraints").innerHTML
-var probOutputFormat = document.getElementById("outputFormat").innerHTML
-var probSampleInput = document.getElementById("sampleInput").innerHTML
-var probSampleOutput = document.getElementById("sampleOutput").innerHTML
-
-function ajaxCall(){
+function debugCall(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -18,7 +9,7 @@ function ajaxCall(){
     xhttp.send();
 }
 
-function updateProblem(){
+function updateProblem1(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
@@ -35,4 +26,47 @@ function updateProblem(){
     };
     xhttp.open("GET", "../testfiles/problem1.txt", true);
     xhttp.send();
+}
+
+function updateProblem2(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var parsedProblem = JSON.parse(this.responseText);
+            document.getElementById("title").innerHTML = parsedProblem.title;
+            document.getElementById("objective").innerHTML = parsedProblem.objective;
+            document.getElementById("task").innerHTML = parsedProblem.task;
+            document.getElementById("inputFormat").innerHTML = parsedProblem.inputFormat;
+            document.getElementById("constraints").innerHTML = parsedProblem.constraints;
+            document.getElementById("outputFormat").innerHTML = parsedProblem.outputFormat;
+            document.getElementById("sampleInput").innerHTML = parsedProblem.sampleInput;
+            document.getElementById("sampleOutput").innerHTML = parsedProblem.sampleOutput;
+        }
+    };
+    xhttp.open("GET", "../testfiles/problem2.txt", true);
+    xhttp.send();
+}
+
+function updateProblem3(){
+    alert("Problem not made yet... check back later.")
+}
+
+function updateProblem4(){
+    alert("Problem not made yet... check back later.")
+}
+
+function updateProblem5(){
+    alert("Problem not made yet... check back later.")
+}
+
+function updateProblem6(){
+    alert("Problem not made yet... check back later.")
+}
+
+function updateProblem7(){
+    alert("Problem not made yet... check back later.")
+}
+
+function updateProblem8(){
+    alert("Problem not made yet... check back later.")
 }
