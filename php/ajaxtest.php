4a53283c5f9b4176aa1c5cfb234502cadc3c7638
Matthew Mello <matthewmello396@gmail.com>
2019-02-19 20:08:01 -0800

    worked on inifinite loop catcher

--- php/ajaxtest.php
+++ php/ajaxtest.php
@@ -1,8 +1,89 @@
-<?php  
+<?php
+    /**
+ * Execute a command and return it's output. Either wait until the command exits or the timeout has expired.
+ *
+ * @param string $cmd     Command to execute.
+ * @param number $timeout Timeout in seconds.
+ * @return string Output of the command.
+ * @throws \Exception
+ */
+function exec_timeout($cmd, $timeout) {
+    // File descriptors passed to the process.
+    $descriptors = array(
+      0 => array('pipe', 'r'),  // stdin
+      1 => array('pipe', 'w'),  // stdout
+      2 => array('pipe', 'w')   // stderr
+    );
+  
+    // Start the process.
+    $process = proc_open($cmd, $descriptors, $pipes);
+  
+    if (!is_resource($process)) {
+      throw new \Exception('Could not execute process');
+    }
+  
+    // Set the stdout stream to none-blocking.
+    stream_set_blocking($pipes[1], 0);
+  
+    // Turn the timeout into microseconds.
+    $timeout = $timeout * 1000000;
+  
+    // Output buffer.
+    $buffer = '';
+  
+    // While we have time to wait.
+    while ($timeout > 0) {
+      $start = microtime(true);
+  
+      // Wait until we have output or the timer expired.
+      $read  = array($pipes[1]);
+      $other = array();
+      stream_select($read, $other, $other, 0, $timeout);
+  
+      // Get the status of the process.
+      // Do this before we read from the stream,
+      // this way we can't lose the last bit of output if the process dies between these     functions.
+      $status = proc_get_status($process);
+  
+      // Read the contents from the buffer.
+      // This function will always return immediately as the stream is none-blocking.
+      $buffer .= stream_get_contents($pipes[1]);
+  
+      if (!$status['running']) {
+        // Break from this loop if the process exited before the timeout.
+        break;
+      }
+  
+      // Subtract the number of microseconds that we waited.
+      $timeout -= (microtime(true) - $start) * 1000000;
+    }
+  
+    // Check if there were any errors.
+    $errors = stream_get_contents($pipes[2]);
+  
+    if (!empty($errors)) {
+      throw new \Exception($errors);
+    }
+  
+    // Kill the process in case the timeout expired and it's still running.
+    // If the process already exited this won't do anything.
+    proc_terminate($process, 9);
+  
+    // Close all streams.
+    fclose($pipes[0]);
+    fclose($pipes[1]);
+    fclose($pipes[2]);
+  
+    proc_close($process);
+  
+    return $buffer;
+  }
+
     // Grabs the code and username from the xmlHTTPrequest variable named code
     // Note: (had to use JS encodeURIcomponent to get it to pass the string correctly)
     $sCode = $_REQUEST["code"];
     // $uName = $_REQUEST["user"];
+    $time = .5;
 
     // hard coded username for development without user system built yet
     $userName = "user1"; // eventually needs to be $uName
@@ -40,6 +121,8 @@
     // to the variable $output. The current returned
     // result is 0, and an executible is created
     $output = system($compileCall, $result);
+
+    $command = "{$fileName}.exe";
     
     // Outputs the errors of compilation to the
     // debug screen if compilation fails. Returns
@@ -48,11 +131,7 @@
         echo $output;
     }
     else{
-        exec("{$fileName}.exe", $returnedResult);
-        foreach($returnedResult as $line){
-            echo $line;
-            echo "\n";
-        }
+        echo exec_timeout($command, $time);
     }
 
     // close file after compilation is complete
